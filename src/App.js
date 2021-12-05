d1db253eee0135fbfc710a01f6b7187e8ccf1674
Nooruddin <nooruddin.rahmani100@gmail.com>
2021-12-05 21:54:15 +0430

    modified app component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+import React, { useState } from "react";
+import "./styles/app.scss";
 import Player from "./components/Player";
 import Song from "./components/Song";
-import "./styles/app.scss";
+import chillHop from "./data";
 function App() {
+  const [songs, setSongs] = useState(chillHop());
+  const [currentSong, setCurrentSong] = useState(songs[0]);
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <div className="App">
-      <Song />
-      <Player />
+      <Song currentSong={currentSong} />
+      <Player
+        setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        currentSong={currentSong}
+      />
     </div>
   );
 }
