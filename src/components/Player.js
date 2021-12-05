e242638666a1bed9af1fb18a14147791b300e185
Nooruddin <nooruddin.rahmani100@gmail.com>
2021-12-05 21:55:28 +0430

    added functionalities to palyer component

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,20 +1,69 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Playarrowroundedicon from "@mui/icons-material/PlayArrowRounded";
+
 import Navigatenextroundedicon from "@mui/icons-material/NavigateNextRounded";
 import Navigatebeforeroundedicon from "@mui/icons-material/NavigateBeforeRounded";
-const Player = () => {
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  const audioRef = useRef(null);
+  const playSongHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+      setIsPlaying(!isPlaying);
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
+  const timeUpdateHandler = (e) => {
+    const current = e.target.currentTime;
+    const duration = e.target.duration;
+    setSongInfo({
+      ...songInfo,
+      currentTime: current,
+      duration,
+    });
+  };
+  const getTime = (time) => {
+    return (
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
+  const dragHandler = (e) => {
+    audioRef.current.currentTime = e.target.value;
+    setSongInfo({ ...songInfo, currentTime: e.target.value });
+  };
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
   return (
     <div className="player">
       <div className="time-control">
-        <p>Start Time</p>
-        <input type="range" />
-        <p>End Time</p>
+        <p>{getTime(songInfo.currentTime)}</p>
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          onChange={dragHandler}
+          type="range"
+        />
+        <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <Navigatebeforeroundedicon className="skip-prev" fontSize="large" />
-        <Playarrowroundedicon className="play" fontSize="large" />
+        <Playarrowroundedicon
+          onClick={playSongHandler}
+          className="play"
+          fontSize="large"
+        />
         <Navigatenextroundedicon className="skip-next" fontSize="large" />
       </div>
+      <audio
+        onTimeUpdate={timeUpdateHandler}
+        onLoadedMetadata={timeUpdateHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };
